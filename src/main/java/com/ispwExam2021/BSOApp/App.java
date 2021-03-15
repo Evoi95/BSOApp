1242a926a02aaf64f3246aeb84357040856eafd5
ginoz <ginoz@Lenovo-14-Gino.station>
2021-03-15 22:36:37 +0100

    po

---
+++ src/main/java/com/ispwExam2021/BSOApp/App.java
@@ -0,0 +1,13 @@
+package com.ispwExam2021.BSOApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/ispwExam2021/BSOApp/AppTest.java
@@ -0,0 +1,20 @@
+package com.ispwExam2021.BSOApp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
